14b6d0c1a8680a9540d301c50065eda584603984
zhangqiang <1069507276@qq.com>
2016-09-12 14:28:08 +0800

    firstCommit

---
+++ Base_Structure_JK/Base_Structure_JK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Base_Structure_JK
+//
+//  Created by zhangqiang on 16/9/12.
+//  Copyright © 2016年 jyfl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Base_Structure_JK/Base_Structure_JK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Base_Structure_JK
+//
+//  Created by zhangqiang on 16/9/12.
+//  Copyright © 2016年 jyfl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
